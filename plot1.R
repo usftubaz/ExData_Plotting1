c3ef86cde673ec69851ecdcd66c82726903cd90c
usftubaz <jonathan.seidel@rivetechnology.com>
2014-05-11 11:21:40 -0400

    Streamlining table read

--- plot1.R
+++ plot1.R
@@ -1,7 +1,6 @@
 plot1 <- function() {
     ##Read file
-    data <- read.table(file="exdata_data_household_power_consumption/household_power_consumption.txt",header=TRUE,sep=";",stringsAsFactors=FALSE)
-    data <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")
+    data <- read.table(file="exdata_data_household_power_consumption/household_power_consumption.txt",header=TRUE,sep=";",stringsAsFactors=FALSE, col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"), skip=66636, nrows=2880)
     data$Date <- as.Date(data$Date, format="%d/%m/%Y")
     data$Global_active_power <- as.numeric(data$Global_active_power)
     
